fe78d7a472fd13ee873cd1d77b2787f83e7f3c7b
Anna Popova <anna_popova1@epam.com>
2020-12-06 19:22:39 +0400

    [M3] Added some unit tests

--- src/app/courses/course-item/course-item.component.spec.ts
+++ src/app/courses/course-item/course-item.component.spec.ts
@@ -1,25 +1,78 @@
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Course, CourseMock } from '../course';
 
 import { CourseItemComponent } from './course-item.component';
 
+const courseItem = new CourseMock('Test title', 'Test description', 100);
+
+@Component({
+  template: `
+    <cl-course-item
+      [course]="course"
+      (deleteRequest)="deleteCourse(course.id)"
+      (editRequest)="editCourse(course)">
+    </cl-course-item>`
+})
+class TestHostComponent {
+  course: Course = courseItem;
+  idToDelete?: string;
+  itemToEdit?: Course;
+  deleteCourse(id: string) {
+    this.idToDelete = id;
+  }
+  editCourse(item: Course) {
+    this.itemToEdit = item;
+  }
+}
+
 describe('CourseItemComponent', () => {
-  let component: CourseItemComponent;
-  let fixture: ComponentFixture<CourseItemComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let courseItemDe: DebugElement;
+  let courseItemEl: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CourseItemComponent ]
+      declarations: [ CourseItemComponent, TestHostComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CourseItemComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    courseItemDe = fixture.debugElement.query(By.css('.course-item'));
+    courseItemEl = courseItemDe.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render course item data', () => {
+    expect(courseItemEl.textContent).toContain(courseItem.title);
+    expect(courseItemEl.textContent).toContain(courseItem.description);
+  });
+
+  it('should handle edit action', () => {
+    const editBtn = courseItemDe.query(By.css('.edit-action'));
+    editBtn.nativeElement.click();
+    expect(component.itemToEdit).toEqual(component.course);
+  });
+
+  // it('should handle delete action', () => {
+  //   spyOn(component.deleteRequest, 'emit');
+  //   const editBtn = courseItemDe.query(By.css('.delete-action'));
+  //   editBtn.nativeElement.click();
+  //   expect(component.deleteRequest.emit).toHaveBeenCalled();
+  // });
+
+  it('should handle delete action', () => {
+    const deleteBtn = courseItemDe.query(By.css('.delete-action'));
+    deleteBtn.nativeElement.click();
+    expect(component.idToDelete).toBe(component.course.id);
   });
 });
