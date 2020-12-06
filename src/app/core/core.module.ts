08b12dd8664b42ab4151f0d635ab89ad362306e7
Anna Popova <anna_popova1@epam.com>
2020-12-06 17:42:18 +0400

    [M2] Moved search panel to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,27 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
     BreadcrumbsComponent,
+    SearchPanelComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatIconModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     BreadcrumbsComponent,
+    SearchPanelComponent,
   ],
 })
 export class CoreModule { }
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CourseItemComponent } from './course-item/course-item.component';
-import { SearchPanelComponent } from './search-panel/search-panel.component';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 
 @NgModule({
   declarations: [
-    SearchPanelComponent,
     CourseItemComponent,
     CoursesPageComponent,
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatIconModule,
